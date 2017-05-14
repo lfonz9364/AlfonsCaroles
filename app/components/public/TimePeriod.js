686b54a924152d1d67be7478a58155af963feb4f
Alfons Caroles <cllin7787@gmail.com>
2017-05-14 16:53:46 +1000

    fix webpack

--- app/components/public/TimePeriod.js
+++ app/components/public/TimePeriod.js
@@ -23,31 +23,31 @@ export default class Timeline extends React.Component {
 
   render(){
       return <div className="timeperiod">
-        <input type="checkbox" value="2017" onChange={this.handleChange} />
+        <input class="circle" type="checkbox" value="2017" onChange={this.handleChange} />
         <label>2017</label>
 
-        <input type="checkbox" value="2016" onChange={this.handleChange} />
+        <input class="circle" type="checkbox" value="2016" onChange={this.handleChange} />
         <label>2016</label>
 
-        <input type="checkbox" value="2015" onChange={this.handleChange} />
+        <input class="circle" type="checkbox" value="2015" onChange={this.handleChange} />
         <label>2015</label>
 
-        <input type="checkbox" value="2014" onChange={this.handleChange} />
+        <input class="circle" type="checkbox" value="2014" onChange={this.handleChange} />
         <label>2014</label>
 
-        <input type="checkbox" value="2013" onChange={this.handleChange} />
+        <input class="circle" type="checkbox" value="2013" onChange={this.handleChange} />
         <label>2013</label>
 
-        <input type="checkbox" value="2012" onChange={this.handleChange} />
+        <input class="circle" type="checkbox" value="2012" onChange={this.handleChange} />
         <label>2012</label>
 
-        <input type="checkbox" value="2011" onChange={this.handleChange} />
+        <input class="circle" type="checkbox" value="2011" onChange={this.handleChange} />
         <label>2011</label>
 
-        <input type="checkbox" value="2010" onChange={this.handleChange} />
+        <input class="circle" type="checkbox" value="2010" onChange={this.handleChange} />
         <label>2010</label>
 
-        <input type="checkbox" value="2009" onChange={this.handleChange} />
+        <input class="circle" type="checkbox" value="2009" onChange={this.handleChange} />
         <label>2009</label>
       </div>
   }
