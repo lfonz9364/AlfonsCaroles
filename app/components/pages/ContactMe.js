563e60f2c54510de59a44a3be9dfaebe7e999034
Alfons Caroles <cllin7787@gmail.com>
2017-05-17 21:48:14 +1000

    Update e-mail

--- app/components/pages/ContactMe.js
+++ app/components/pages/ContactMe.js
@@ -9,7 +9,7 @@ export default class ContactMe extends React.Component {
   render(){
       return <div className="contact" >
         <h2>Please select your preferred contact method</h2>
-					<a href="mailto:cllin7787@gmail.com?subject=OnlinePortfolio"><img src="./images/mail.png" /></a>
+					<a href="mailto:alfons.caroles@gmail.com?subject=OnlinePortfolio"><img src="./images/mail.png" /></a>
           <a href="https://www.linkedin.com/in/alfonscaroles/" target="_blank"><img src="./images/linkedin.png" /></a>
       </div>
   }
